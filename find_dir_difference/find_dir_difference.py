5037bccaf3747fdcc1d118f5cec6239cba9e92d2
Sun Hao-B55235 <b55235@freescale.com>
2015-12-01 15:26:55 +0800

    Add find_dir_difference.py 1. The script is used to find the difference between two directors.
    Signed-off-by: Sun Hao-B55235 <b55235@freescale.com>

---
+++ find_dir_difference/find_dir_difference.py
@@ -0,0 +1,74 @@
+# !/ usr / bin / python
+# Filename : find_dir_difference.py
+# command line example:
+#   python find_dir_difference.py 
+
+import re,os,sys,time,subprocess
+
+dir_a = 'E:\\sun_project\\python_test\\test0'
+dir_b = 'E:\\sun_project\\python_test\\test1'
+
+is_same = True
+
+both_have = []
+only_a_have = []
+only_b_have = []
+
+dir_a_list = dir_a.split('\\')
+dir_b_list = dir_b.split('\\')
+
+for parent,dirnames,filenames in os.walk(dir_a):
+    for filename in filenames:
+        filename_path = os.path.join(parent,filename)
+        dir_b_copy_list = dir_b_list[:]
+        
+        # Get the corresponding file path in dir b 
+        dir_a_file_path_list = filename_path.split('\\')
+        dir_a_file_path_alone_list = [i for i in dir_a_file_path_list if i not in dir_a_list]
+        dir_b_copy_list.extend(dir_a_file_path_alone_list)
+        dir_b_possible_path = "\\".join(dir_b_copy_list)
+        dir_file_path_alone_list = dir_a_file_path_alone_list[:]
+        dir_file_path_alone = "\\".join(dir_file_path_alone_list)
+        
+        # Whether the file exits
+        if os.path.exists(dir_b_possible_path) == True:
+            both_have.append(dir_file_path_alone)
+        else:
+            is_same = False
+            only_a_have.append(dir_file_path_alone)
+
+for parent,dirnames,filenames in os.walk(dir_b):
+    for filename in filenames:
+        filename_path = os.path.join(parent,filename)
+        dir_a_copy_list = dir_a_list[:]
+        
+        # Get the corresponding file path in dir b 
+        dir_b_file_path_list = filename_path.split('\\')
+        dir_b_file_path_alone_list = [i for i in dir_b_file_path_list if i not in dir_b_list]
+        dir_a_copy_list.extend(dir_b_file_path_alone_list)
+        dir_a_possible_path = "\\".join(dir_a_copy_list)
+        dir_file_path_alone_list = dir_b_file_path_alone_list[:]
+        dir_file_path_alone = "\\".join(dir_file_path_alone_list)
+        
+        # Whether the file exits
+        if os.path.exists(dir_a_possible_path) == True:
+            both_have.append(dir_file_path_alone)
+        else:
+            is_same = False
+            only_b_have.append(dir_file_path_alone)
+
+if is_same == True:
+    print 'The two directors are the same.'
+else:
+    if len(only_a_have):
+        print 78*'*'
+        print 'The following files only exit in dir a: '
+        for mem in only_a_have:
+            print mem
+    
+    if len(only_b_have):
+        print 78*'*'
+        print 'The following files only exit in dir b: '
+        for mem in only_b_have:
+            print mem
+
