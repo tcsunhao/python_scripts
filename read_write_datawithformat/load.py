b9beb622b6bf31bf210d4199b419129c88d99789
Sun Hao-B55235 <b55235@freescale.com>
2016-01-09 11:01:50 +0800

    Add checklist_sdk
    Signed-off-by: Sun Hao-B55235 <b55235@freescale.com>

---
+++ read_write_datawithformat/load.py
@@ -0,0 +1,71 @@
+# !/usr/bin/python
+# Filename : load.py
+
+import re, os, sys, time, subprocess, fnmatch, json, yaml
+
+############################
+## The typical way to load a json file
+# f = file('default_config.json');
+# s = json.load(f)
+# f.close()
+
+# for key in s.keys():
+#     print s.get(key,'')
+
+
+############################
+## The typical way to load yaml file
+# feature = {'FSL_FEATURE_SOC_CMP_COUNT': True,
+#            'FSL_FEATURE_SOC_EMVSIM_COUNT':True,
+#            'FSL_FEATURE_SOC_XBAR_COUNT':2}
+
+# driver_module_list = []
+# example_driver_dic = {}
+
+# ## Input the yml
+# f = file('demo_app.yml')
+# yf = yaml.load(f)
+
+# for item in yf.items():
+#     tmp_driver_module_list = item[1]['modules'].keys()
+#     if len(tmp_driver_module_list) != 0:
+#         for mem in tmp_driver_module_list:
+#             if (mem=='pinset' or mem=='clock' or mem=='demo' or mem=='example' 
+#                 or mem=='demo' or mem==('fsl_'+item[0]) or mem=='port' or mem=='uart' or mem=='mcglite'
+#                 or mem=='lpuart' or mem=='lpsci' or mem=='gpio' or mem=='sim' or mem=='osc' or mem=='mcg'):
+#                 pass
+#             else:
+#                 driver_module_list.append(mem)
+#     example_driver_dic[item[0]] = driver_module_list
+#     driver_module_list = []
+
+# items = example_driver_dic.items()
+# items.sort()
+
+# f = open('map_driver2demo_apps.yml','w')
+
+# for key,value in items:
+#     print >> f, key + ':'
+#     print >> f, '    or:'
+#     print >> f, '    and:'
+#     value.sort()
+#     for mem in value:
+#         print >> f, '        ' + mem
+#     print >> f, '\n',
+
+# f.close()
+
+#### Output the yml
+# f = file('newdata.yaml','w')
+# yaml.dump(example_driver_dic,f)
+# f.close()
+
+###############################
+### read and write excel
+
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('zm6.xlsx')
+worksheet = workbook.add_worksheet('sheet No.1')
+worksheet.write(0, 0, 'hello world')
+workbook.close()
